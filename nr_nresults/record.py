155f548c6bd6cca1c684116336ca155aa26b572a
Daniel Kopecký <kopecky.d@gmail.com>
2020-12-08 10:08:40 +0100

    Implemented CanonicalUrlMixin

--- nr_nresults/record.py
+++ nr_nresults/record.py
@@ -1,5 +1,5 @@
-from flask import url_for
 from invenio_records.api import Record
+from nr_common.record import CanonicalUrlMixin
 from oarepo_references.mixins import ReferenceEnabledRecordMixin
 from oarepo_validate import SchemaKeepingRecordMixin, MarshmallowValidatedRecordMixin
 
@@ -8,14 +8,15 @@ from nr_nresults.marshmallow import NResultsMetadataSchemaV1
 
 
 class PublishedNResultRecord(SchemaKeepingRecordMixin,
-                           MarshmallowValidatedRecordMixin,
-                           ReferenceEnabledRecordMixin,
-                           Record):
+                             MarshmallowValidatedRecordMixin,
+                             ReferenceEnabledRecordMixin,
+                             CanonicalUrlMixin,
+                             Record):
     ALLOWED_SCHEMAS = NRESULTS_ALLOWED_SCHEMAS
     PREFERRED_SCHEMA = NRESULTS_PREFERRED_SCHEMA
     MARSHMALLOW_SCHEMA = NResultsMetadataSchemaV1
 
     @property
     def canonical_url(self):
-        return url_for('invenio_records_rest.nresults_item',
-                       pid_value=self['control_number'], _external=True)
+        return self.get_canonical_url('invenio_records_rest.nresults_item',
+                                      pid_value=self['control_number'], _external=True)
